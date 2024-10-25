bed77f260fd73bf7b77ba533a64f17748353c546
mdaudinot <mdaudinot@soaint.com>
2024-10-25 06:06:47 -0400

    camel-microservice-b v0.1

---
+++ src/test/java/com/camel/microservices_b/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.microservices_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
